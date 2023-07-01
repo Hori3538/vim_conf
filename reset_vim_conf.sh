502c92687bd2bda7198fab4db9f38543ce53edf1
Hori3538 <yu.m.h.94612@gmail.com>
2023-07-02 02:01:29 +0900

    minor update

--- reset_vim_conf.sh
+++ reset_vim_conf.sh
@@ -7,7 +7,13 @@ ln -sf $SCRIPT_DIR/vimrc ~/.vimrc
 ln -sf $SCRIPT_DIR/coc-settings.json ~/.vim/
 
 color_files="$SCRIPT_DIR/colors/*"
+
+VIM_COLOR_DIR=~/.vim/colors
+if [ ! -d $VIM_COLOR_DIR ]; then
+  mkdir -p $VIM_COLOR_DIR
+else
+
 for color_file in $color_files; do
     echo `basename $color_file`
-    ln -sf $color_file ~/.vim/colors/`basename ${color_file}`
+    ln -sf $color_file ${VIM_COLOR_DIR}/`basename ${color_file}`
 done
